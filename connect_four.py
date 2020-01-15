1abe3730ed0395e57376c08033f3c5234905aa98
Ellen <eyusti@gmail.com>
2020-01-15 11:18:10 -0500

    Everything but tests for winner

--- connect_four.py
+++ connect_four.py
@@ -1,4 +1,122 @@
+from pprint import pprint
+from random import randint
 
 class Board:
     def __init__(self):
-        self.board = [[[] for column in range(1,8)]for row in range(1,7)]
+        self.board = [["?" for column in range(7)]for row in range(6)]
+
+    def print_board(self):
+        pprint(self.board)
+
+    def board_is_full(self):
+        for row in range(6):
+            for column in range(7):
+                #print(self.board[row][column])
+                if self.board[row][column] == "?":
+                    return False
+        return True
+    
+    def does_column_have_space(self, column):
+        for row in range(6):
+            if self.board[row][column] == "?":
+                return True
+        return False
+
+    def place(self, column, player_color):
+        for row in range(5,-1,-1):
+            if self.board[row][column] == "?":
+                self.board[row][column] = player_color
+                return
+
+
+class Game:
+    def __init__(self):
+        self.player1 = None
+        self.p1_color = "R"
+        self.player2 = None
+        self.p2_color = "B"
+        self.current_turn = 0
+        self.board = Board()
+
+    def get_player_AI(self):
+        print("Welcome to Connect Four! What AIs would you like to play against each other?")
+        while True:
+            try: 
+                self.player1 = input("Which AI would you like for player 1? (RNG / Negamax) ").lower()
+                while self.player1 not in ["rng","negamax"]:
+                    self.player1 = input("Sorry, that isn't an AI we have. Which AI would you like for player 1? (RNG / Negamax) ").lower()     
+            except ValueError:
+                print("I'm sorry Dave, I'm afraid I can't do that")
+                continue
+            else:
+                break
+        while True:
+            try: 
+                self.player2 = input("Which AI would you like for player 2? (RNG / Negamax) ").lower()
+                while self.player2 not in ["rng","negamax"]:
+                    self.player2 = input("Sorry, that isn't an AI we have. Which AI would you like for player 2? (RNG / Negamax) ").lower()     
+            except ValueError:
+                print("I'm sorry Dave, I'm afraid I can't do that")
+                continue
+            else:
+                break
+    
+    def switch_current_player(self):
+        if self.current_turn == 0:
+            self.current_turn = 1
+            return
+        if self.current_turn == 1:
+            self.current_turn = 0
+            return
+
+    def rng_move(self):
+        column = randint(0,6)
+        return column
+
+    def check_four_consecutive(self,any_list):
+        visited_list = []
+        visited_list.append(any_list[0])
+        for item in any_list[1:]:
+            if item in visited_list:
+                visited_list.append(item)
+                if len(visited_list) >= 4:
+                    return True
+            if item not in visited_list:
+                visited_list = []
+                visited_list.append(item)
+        return False
+
+    def check_row(self):
+        for row in range(6):
+            if self.check_four_consecutive(self.board.board[row]):
+                return True
+        return False
+
+    def play_game(self):        
+        while not self.board.board_is_full():
+            current_AI = None
+            current_color = None
+
+            if self.current_turn == 0:
+                current_AI = self.player1
+                current_color = self.p1_color
+            if self.current_turn == 1:
+                current_AI = self.player2
+                current_color = self.p2_color
+            
+            if current_AI == "rng":
+                while True:
+                    column = self.rng_move()
+                    if self.board.does_column_have_space(column):
+                        self.board.place(column, current_color)
+                        break
+            
+            self.switch_current_player()
+            self.board.print_board()
+
+            """if current_AI == "Negamax":
+                row, column, score = Negamax()
+                place(row, column, current_color)"""
+
+new_game = Game()
+print(new_game.check_row())
--- connect_four_test.py
+++ connect_four_test.py
@@ -1,1 +1,35 @@
-# Board should be 7 columns by 6 rows
+import pytest
+from connect_four import Board, Game
+
+def test_board_is_full_with_full_board():
+    new_board = Board()
+    new_board.board = [["R" for column in range(7)]for row in range(6)]
+    assert new_board.board_is_full() == True
+
+def test_board_is_full_with_not_full_board():
+    new_board = Board()
+    new_board.board = [["?" for column in range(7)]for row in range(6)]
+    assert new_board.board_is_full() == False
+
+def test_position_is_empty_while_empty():
+    new_board = Board()
+    new_board.board = [["?" for column in range(7)]for row in range(6)]
+    assert new_board.is_position_empty(1,1) == True
+
+def test_position_is_empty_while_full():
+    new_board = Board()
+    new_board.board = [["R" for column in range(7)]for row in range(6)]
+    assert new_board.is_position_empty(1,1) == False
+
+def test_place_works_correctly():
+    new_board = Board()
+    new_board.place(1,1,"R")
+    assert new_board.board == [['?', '?', '?', '?', '?', '?', '?'],['?', "R", '?', '?', '?', '?', '?'],['?', '?', '?', '?', '?', '?', '?'],['?', '?', '?', '?', '?', '?', '?'],['?', '?', '?', '?', '?', '?', '?'],['?', '?', '?', '?', '?', '?', '?']]
+
+def test_get_player_AI():
+    game = Game()
+    game.input = lambda: 'some input'
+
+    game.get_player_AI()
+
+    assert game.player1 != None and game.player2 != None
