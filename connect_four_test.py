5578f00eeebe6e03dd6c4a23c06ba5843f6016a3
Ellen <eyusti@gmail.com>
2020-01-27 14:33:57 -0500

    Updated test cases to test all loops in diagonal win check

--- connect_four_test.py
+++ connect_four_test.py
@@ -4,7 +4,7 @@ from connect_four import Board, Game
 # Board test cases
 def test_board_is_full_with_full_board():
     new_board = Board()
-    new_board.board = [["R" for column in range(7)]for row in range(6)]
+    new_board.board = [["1" for column in range(7)]for row in range(6)]
     assert new_board.board_is_full() == True
 
 def test_board_is_full_with_not_full_board():
@@ -12,7 +12,7 @@ def test_board_is_full_with_not_full_board():
     new_board.board = [["?" for column in range(7)]for row in range(6)]
     assert new_board.board_is_full() == False
 
-# Game test cases
+# Win checking test cases
 def test_row_not_win():
     new_game = Game()
     new_game.board.board = [
@@ -21,8 +21,8 @@ def test_row_not_win():
         ['?', '?', '?', '?', '?', '?', '?'],
         ['?', '?', '?', '?', '?', '?', '?'],
         ['?', '?', '?', '?', '?', '?', '?'],
-        ['R', 'B', 'R', 'B', 'R', 'B', 'R']]
-    assert new_game.check_row() == False
+        ['2', '1', '2', '1', '2', '1', '2']]
+    assert new_game.check_row() == None
     
 def test_row_win():
     new_game = Game()
@@ -32,41 +32,74 @@ def test_row_win():
         ['?', '?', '?', '?', '?', '?', '?'],
         ['?', '?', '?', '?', '?', '?', '?'],
         ['?', '?', '?', '?', '?', '?', '?'],
-        ['B', 'B', 'R', 'R', 'R', 'R', 'B']]
-    assert new_game.check_row() == True
+        ['1', '1', '2', '2', '2', '2', '1']]
+    assert new_game.check_row() == "2"
 
 def test_column_win():
     new_game = Game()
     new_game.board.board = [
         ['?', '?', '?', '?', '?', '?', '?'],
         ['?', '?', '?', '?', '?', '?', '?'],
-        ['?', '?', '?', '?', '?', '?', 'B'],
-        ['?', '?', '?', '?', '?', 'R', 'B'],
-        ['?', '?', '?', '?', '?', 'R', 'B'],
-        ['?', '?', '?', '?', '?', 'R', 'B']]
-    assert new_game.check_column() == True
+        ['?', '?', '?', '?', '?', '?', '1'],
+        ['?', '?', '?', '?', '?', '2', '1'],
+        ['?', '?', '?', '?', '?', '2', '1'],
+        ['?', '?', '?', '?', '?', '2', '1']]
+    assert new_game.check_column() == "1"
 
 def test_column_not_win():
     new_game = Game()
     new_game.board.board = [
         ['?', '?', '?', '?', '?', '?', '?'],
         ['?', '?', '?', '?', '?', '?', '?'],
-        ['?', '?', '?', '?', '?', '?', 'R'],
-        ['?', '?', '?', '?', '?', '?', 'B'],
-        ['?', '?', '?', '?', '?', 'R', 'B'],
-        ['?', '?', '?', '?', '?', 'R', 'B']]
-    assert new_game.check_column() == False
+        ['?', '?', '?', '?', '?', '?', '2'],
+        ['?', '?', '?', '?', '?', '?', '1'],
+        ['?', '?', '?', '?', '?', '2', '1'],
+        ['?', '?', '?', '?', '?', '2', '1']]
+    assert new_game.check_column() == None
 
 def test_diagonal_win():
     new_game = Game()
     new_game.board.board = [
         ['?', '?', '?', '?', '?', '?', '?'],
         ['?', '?', '?', '?', '?', '?', '?'],
-        ['?', '?', '?', '?', '?', '?', 'B'],
-        ['?', '?', '?', '?', '?', 'B', 'R'],
-        ['?', '?', '?', '?', 'B', 'R', 'B'],
-        ['?', '?', '?', 'B', 'R', 'R', 'R']]
-    assert new_game.check_diagonals() == True
+        ['?', '?', '?', '?', '?', '?', '1'],
+        ['?', '?', '?', '?', '?', '1', '2'],
+        ['?', '?', '?', '?', '1', '2', '1'],
+        ['?', '?', '?', '1', '2', '2', '2']]
+    assert new_game.check_diagonals() == "1"
+
+def test_diagonal_win_other_loop():
+    new_game = Game()
+    new_game.board.board = [
+        ['?', '?', '?', '1', '?', '2', '1'],
+        ['?', '?', '1', '?', '2', '1', '?'],
+        ['?', '1', '?', '?', '?', '?', '1'],
+        ['1', '?', '2', '1', '?', '?', '2'],
+        ['?', '?', '?', '?', '1', '2', '1'],
+        ['?', '?', '?', '?', '2', '2', '2']]
+    assert new_game.check_diagonals() == "1"
+
+def test_other_diagonal_win():
+    new_game = Game()
+    new_game.board.board = [
+        ['?', '?', '?', '1', '?', '?', '?'],
+        ['?', '?', '?', '?', '1', '?', '?'],
+        ['1', '?', '?', '?', '?', '1', '?'],
+        ['2', '?', '?', '?', '?', '?', '1'],
+        ['1', '2', '1', '?', '?', '?', '?'],
+        ['2', '2', '2', '1', '?', '?', '?']]
+    assert new_game.check_diagonals() == "1"
+
+def test_other_diagonal_win_other_loop():
+    new_game = Game()
+    new_game.board.board = [
+        ['?', '?', '?', '?', '?', '?', '?'],
+        ['?', '?', '?', '?', '?', '?', '?'],
+        ['1', '?', '?', '?', '?', '?', '?'],
+        ['2', '1', '?', '?', '?', '?', '?'],
+        ['1', '2', '1', '?', '?', '?', '?'],
+        ['2', '2', '2', '1', '?', '?', '?']]
+    assert new_game.check_diagonals() == "1"
 
 def test_diagonal_not_win():
     new_game = Game()
@@ -74,28 +107,29 @@ def test_diagonal_not_win():
         ['?', '?', '?', '?', '?', '?', '?'],
         ['?', '?', '?', '?', '?', '?', '?'],
         ['?', '?', '?', '?', '?', '?', '?'],
-        ['?', '?', '?', '?', '?', 'B', 'R'],
-        ['?', '?', '?', '?', 'B', 'R', 'B'],
-        ['?', '?', '?', 'B', 'R', 'R', 'R']]
-    assert new_game.check_diagonals() == False
+        ['?', '?', '?', '?', '?', '1', '2'],
+        ['?', '?', '?', '?', '1', '2', '1'],
+        ['?', '?', '?', '1', '2', '2', '2']]
+    assert new_game.check_diagonals() == None
 
-def test_other_diagonal_win():
+def test_provide_winner():
     new_game = Game()
     new_game.board.board = [
         ['?', '?', '?', '?', '?', '?', '?'],
         ['?', '?', '?', '?', '?', '?', '?'],
-        ['B', '?', '?', '?', '?', '?', '?'],
-        ['R', 'B', '?', '?', '?', '?', '?'],
-        ['B', 'R', 'B', '?', '?', '?', '?'],
-        ['R', 'R', 'R', 'B', '?', '?', '?']]
-    assert new_game.check_diagonals() == True
+        ['?', '?', '?', '?', '?', '?', '1'],
+        ['?', '?', '?', '?', '?', '1', '2'],
+        ['?', '?', '?', '?', '1', '2', '1'],
+        ['?', '?', '?', '1', '2', '2', '2']]
+    assert new_game.provide_winner() == "1"
 
-@pytest.mark.skip(reason="unfinished test")
-def test_get_player_AI():
-    game = Game()
-    game.input = lambda: 'some input'
-
-    game.get_player_AI()
-
-    assert game.player1 != None and game.player2 != None
-
+def test_provide_winner_no_winner():
+    new_game = Game()
+    new_game.board.board = [
+        ['?', '?', '?', '?', '?', '?', '?'],
+        ['?', '?', '?', '?', '?', '?', '?'],
+        ['?', '?', '?', '?', '?', '?', '?'],
+        ['?', '?', '?', '?', '?', '1', '2'],
+        ['?', '?', '?', '?', '1', '2', '1'],
+        ['?', '?', '?', '1', '2', '2', '2']]
+    assert new_game.provide_winner() == None
