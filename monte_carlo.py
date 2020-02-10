1ac44b46c8417484439c7a7abc5ec3700c93bd4f
Ellen <eyusti@gmail.com>
2020-02-10 16:25:12 -0500

    Added traverse and traverse helper methods

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,47 +1,44 @@
 from copy import deepcopy
 from connect_four import Board
 from math import sqrt , log, inf
+from random import randint
 
-def monte_carlo_tree_search(root, current_player):
+def monte_carlo_tree_search(current_board, current_player):
+    #need to replace this with a better metric for resources left
     num_rollouts = 1000
-    best_move = None
+    root = Node(None, board = current_board)
 
     while num_rollouts > 0 :
         node_to_expand = traverse(root)
         score = rollout(node_to_expand)
         backpropogate_scores(node_to_expand, score)
+        
+        num_rollouts -= 1
     
     return best_move(root)
 
 # Main Methods
-def traverse(root):
-    current_node = root
-    temp_node = None
-    while not current_node.is_leaf():
-        max_score = -inf
-        for child in current_node.children:
-            score = ucb_scoring(child)
-            if score > max_score:
-                max_score = score
-                temp_node = child
-        current_node = temp_node
-    return current_node
+def traverse(node):
+    while fully_expanded(node):
+        node = best_ucb(node)
+    return pick_unvisited(node) or node
 
 def rollout(node):
-    score = None
-    return score
+    while not_terminal(node):
+        node = rollout_policy(node)
+    return result(node)
 
 def backpropogate_scores(node, score):
     pass
 
-# Helper Methods
+### Helper Methods ###
 class Node:
     def __init__(self, move, board = Board(), score = 0,times_visited = 0, parent_node = None):
         self.score = score
         self.times_visited = times_visited
         self.children = []
         self.board = board
-        self.move_column = move
+        self.column = move
         self.parent_node = parent_node
     
     def add_child(self,child_node):
@@ -58,15 +55,41 @@ class Node:
     def is_leaf(self):
         return not self.children 
 
+# Tree Helpers
 def best_move(root):
     best_column = None
     most_visited = -inf
     for node in root.children:
         if node.times_visited > most_visited:
             most_visited = node.times_visited
-            best_column = node.move_column
+            best_column = node.column
     return best_column
 
+# Traverse Helpers
 def ucb_scoring(node):
     score = (node.score/node.times_visited) + 2 * sqrt(log(node.parent_node.times_visited)/node.times_visited)
     return score
+
+def best_ucb(node):
+    max_score = -inf
+    best_node = None
+    for child in node.children:
+        score = ucb_scoring(child)
+        if score > max_score:
+            max_score = score
+            best_node = child
+    return best_node
+
+def fully_expanded(node):
+    for child in node.children:
+        if child.times_visited == 0:
+            return False
+    return True
+
+def pick_unvisited(node):
+    possible_children = []
+    for child in node.children:
+        if child.times_visited == 0:
+            possible_children.append(child)
+    index = randint(0, len(possible_children)-1)
+    return possible_children[index]
--- monte_carlo_test.py
+++ monte_carlo_test.py
@@ -1,6 +1,6 @@
 import pytest
 from connect_four import Board
-from monte_carlo import Node, ucb_scoring, traverse, best_move
+from monte_carlo import Node, ucb_scoring, traverse, best_move, best_ucb, fully_expanded, pick_unvisited, traverse
 
 board = Board()
 board.board = [
@@ -13,6 +13,7 @@ board.board = [
 board_move = 1
 board_node = Node(board_move, board)
 
+# Node Tests
 def test_constructor_defaults():
     move = None
     new_node = Node(move)
@@ -36,6 +37,30 @@ def test_get_all_children():
     board_node.get_children_nodes("2")
     assert len(board_node.children) == 7
 
+# Main Tree Search
+def test_best_move():
+    root = Node(None)
+
+    node1 = Node(None, times_visited = 50)
+    node1.column = 0
+
+    node2 = Node(None, times_visited = 100)
+    node2.column = 1
+
+    node3 = Node(None, times_visited = 734)
+    node3.column = 2
+
+    node4 = Node(None, times_visited = 42)
+    node4.column = 3
+
+    root.children.append(node1)
+    root.children.append(node2)
+    root.children.append(node3)
+    root.children.append(node4)
+
+    assert best_move(root) == 2
+
+# Traverse Helpers
 def test_ucb_score():
     root = Node (None,score = 30, times_visited = 2 )
     test_node = Node(None,score = 20, times_visited = 1)
@@ -47,35 +72,71 @@ def test_ucb_score():
     assert round(ucb_scoring(test_node),2) == 21.67
     assert round(ucb_scoring(test_node_two),2) == 11.67
 
-#this function definitely needs to do something different than what i think it should do
-def test_traverse():
-    root = Node (None,score = 30, times_visited = 2 )
+def test_best_ucb():
+    root = Node(None,score = 30, times_visited = 2 )
     test_node = Node(None,score = 20, times_visited = 1)
     test_node.parent_node = root
     test_node_two = Node(None, score = 10, times_visited = 1)
     test_node_two.parent_node = root
     root.children.append(test_node)
     root.children.append(test_node_two)
-    assert traverse(root) == test_node
+    assert best_ucb(root) == test_node
 
-def test_best_move():
+def test_fully_expanded_true():
     root = Node(None)
-
     node1 = Node(None, times_visited = 50)
-    node1.move_column = 0
-
     node2 = Node(None, times_visited = 100)
-    node2.move_column = 1
-
     node3 = Node(None, times_visited = 734)
-    node3.move_column = 2
-
     node4 = Node(None, times_visited = 42)
-    node4.move_column = 3
-
     root.children.append(node1)
     root.children.append(node2)
     root.children.append(node3)
     root.children.append(node4)
+    assert fully_expanded(root) == True
 
-    assert best_move(root) == 2
+def test_fully_expanded_false():
+    root = Node(None)
+    node1 = Node(None, times_visited = 50)
+    node2 = Node(None, times_visited = 0)
+    node3 = Node(None, times_visited = 734)
+    node4 = Node(None, times_visited = 42)
+    root.children.append(node1)
+    root.children.append(node2)
+    root.children.append(node3)
+    root.children.append(node4)
+    assert fully_expanded(root) == False
+
+def test_pick_unvisited():
+    root = Node(None)
+    node1 = Node(None, times_visited = 50)
+    node2 = Node(None, times_visited = 0)
+    node3 = Node(None, times_visited = 734)
+    node4 = Node(None, times_visited = 0)
+    root.children.append(node1)
+    root.children.append(node2)
+    root.children.append(node3)
+    root.children.append(node4)
+    assert pick_unvisited(root) in [node2,node4]
+
+def test_traverse():
+    root = Node(None,score = 30, times_visited = 2 )
+
+    test_node = Node(None,score = 20, times_visited = 1)
+    test_node.parent_node = root
+    root.children.append(test_node)
+
+    test_node_1 = Node(None, times_visited = 1)
+    test_node_2 = Node(None, times_visited = 1)
+    test_node_3 = Node(None, times_visited = 0)
+    test_node_4 = Node(None, times_visited = 1)
+
+    test_node.children.append(test_node_1)
+    test_node.children.append(test_node_2)
+    test_node.children.append(test_node_3)
+    test_node.children.append(test_node_4)
+
+    test_node_two = Node(None, score = 10, times_visited = 1)
+    test_node_two.parent_node = root
+    root.children.append(test_node_two)
+
+    assert traverse(root) == test_node_3
